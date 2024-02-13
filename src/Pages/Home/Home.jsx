bb438d0c5bb1a79e90a104837efe3c098e6c2122
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-13 12:23:57 +0600

    cards imported to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,16 @@
+import Cards from '../../Components/Cards/Cards';
+
 const Home = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
